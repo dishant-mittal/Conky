9689b7363cf3cf6a1a5a22a8cd615fb25b92bd19
dishant <dishantmittal@yahoo.co.in>
2017-12-24 13:23:42 -0500

    reading text file to do list

--- startups.sh
+++ startups.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo chmod +x /home/dishant/Projects/initializations/startups.sh
-sudo chmod 755 /home/dishant/Projects/initializations/startups.sh
+sudo chmod +x /home/dishant/Projects/conky/startups.sh
+sudo chmod 755 /home/dishant/Projects/conky/startups.sh
+pkill conky
 
-sleep 12
+sleep 25
 conky;
